8bc792777a9143daf0682c862c44f727ba40d87d
chenwei68 <chenwei68@meituan.com>
2020-08-02 09:14:08 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//element插件配置-步骤2-引入配置插件js
+import './plugins/element.js'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div id="home">
+    <el-breadcrumb-item>
+    <a>  线上发布</a>
+    </el-breadcrumb-item>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <img src="../../assets/logo.png">
+    <router-view/>
+      <div>
+      <p>sfsgsg</p>
+  </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+#home {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
